6fe00a84b5ea9d981c7d6459e890a48ac0d5e38f
swaption2009 <swaption2009@gmail.com>
2016-09-19 13:35:06 -0700

    add search by tags and tag cloud

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,14 @@ class Event < ApplicationRecord
   belongs_to :organizers, class_name: 'User', optional: true
   has_many :tags, through: :taggings
 
+  def self.tagged_with(name)
+    Tag.find_by_name!(name).events
+  end
+
+  def self.tag_counts
+    Tag.select('tags.name, count(taggins.tag_id' as count).joins(:taggings).group('taggings.tag_id')
+  end
+
   def all_tags
     tags.map(&:name).join(", ")
   end
